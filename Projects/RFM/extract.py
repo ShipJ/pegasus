684eec11583d662238efc972d84727df6ce67627
Jack Shipway <jack.shipway@ascential.com>
2017-10-10 18:09:21 +0100

    Connected to salesforce and wham data. File logic works well.

--- Projects/RFM/extract.py
+++ Projects/RFM/extract.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import Projects.RFM.gbq as gbq
 import Projects.RFM.analyse as an
+from Config.structure import get_path, file_check, save_df, get_df
 
 
 def main():
@@ -8,13 +10,33 @@ def main():
     project = 'hypnotic-bounty-755'
     # How many days over which to compute RFM
     days = gbq.get_days()
-    # Specific dates of each day (for BQ table name)
+    # Specific dates of each day (for BQ table names)
     dates = gbq.get_dates(days)
     # Compute model over given range
-    results = an.aggregate_rfm(project, dates)
+    agg = an.aggregate_rfm(project, dates)
     # compute recency, frequency etc
+    rfm = an.rf_compute(agg)
 
-    print results
+
+    # Grab data
+    path = get_path()
+    # Wham
+    extension = '/Raw/wham'
+    wham = get_df(path+extension)
+    # Salesforce
+    extension = '/Raw/salesforce'
+    sf = get_df(path + extension)
+
+
+    # Merge the data on Wham ID, then join with salesforce.
+    df = rfm.merge(wham)
+    df = rfm.merge(sf)
+
+
+    # Save results to file
+    save_df(data, path, extension)
+
+
 
 
 if __name__ == '__main__':
