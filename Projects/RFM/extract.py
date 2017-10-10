6bb6ed88877aeb6ad3acc2e8d7218cd42606ba3e
Jack Shipway <jack.shipway@ascential.com>
2017-10-10 13:34:07 +0100

    Created main script and siphoned functions into appropriate files.

--- Projects/RFM/extract.py
+++ Projects/RFM/extract.py
@@ -1,11 +1,25 @@
 import sys
-
+import Projects.RFM.gbq as gbq
+import Projects.RFM.analyse as an
 from Config.structure import get_path
 from Config.security import get_pwd
 
 
+
 def main():
-    print 'Hello World!'
+
+    # BQ Project Name
+    project = 'hypnotic-bounty-755'
+    # How many days over which to compute RFM
+    days = gbq.get_days()
+    # Specific dates of each day (for BQ table name)
+    dates = gbq.get_dates(days)
+    # Compute model over given range
+    results = an.aggregate_rfm(project, dates)
+    # compute recency, frequency etc
+
+    path = get_path()
+
 
 
 if __name__ == '__main__':
