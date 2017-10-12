00e2646d5aeeb03a27abffd85046d2a0125abda9
Jack Shipway <jack.shipway@ascential.com>
2017-10-12 13:05:45 +0100

    Simplified script, returns data successfully.

--- Projects/RFM/extract.py
+++ Projects/RFM/extract.py
@@ -1,26 +1,5 @@
-import pandas as pd
-import Projects.RFM.gbq as gbq
-import Projects.RFM.analyse as an
-from Config.structure import get_path, file_check, save_df, get_df
-
-
-def big_query():
-    print 'Obtaining Big Query Data...\n---------------------------\n'
-    # BQ Project Name
-    project = 'hypnotic-bounty-755'
-    # How many days over which to compute RFM
-    days = gbq.get_days()
-    # Specific dates of each day (for BQ table names)
-    dates = gbq.get_dates(days)
-    # Compute model over given range9
-    agg = an.aggregate(project, dates)
-    return agg
-
-
-def salesforce(path):
-    print 'Obtaining Salesforce Data\n-------------------------\n'
-    sf = get_df(path+'/Raw/salesforce_2.0')
-    return sf
+from Config.structure import get_path
+from Projects.RFM.extract_functions import big_query, salesforce
 
 
 def main():
@@ -30,17 +9,8 @@ def main():
     bq = big_query()
     sf = salesforce(path)
 
-
-
-
-
-
-
-
-    # compute recency, frequency etc
-    # rfm = an.rf_compute(agg)
-
-
+    print bq
+    print sf
 
 
 
--- Projects/RFM/extract_functions.py
+++ Projects/RFM/extract_functions.py
@@ -1,9 +1,63 @@
 import os
 import pandas as pd
 import pandas_gbq as gbq
-import pymysql.cursors
+from Config.structure import get_df
 from datetime import datetime, date, timedelta
-pd.set_option('display.width', 320)
+
+
+def aggregate(project, date_range):
+    """
+    For each day in the date range inputted above, request big query for the data and concat all results
+    :param project: Str - Name of the project within BQ
+    :param date_range: List of dates in yyyyddmm format, required for identifying each BQ table
+    :return: 
+    """
+    results = []
+    for date in date_range:
+        query = bq_query(date)
+        result = bq_getdata(project, query)
+        results.append(result)
+    results = pd.DataFrame(pd.concat(results))
+    results = pd.DataFrame(results.groupby('WhamId').agg({'Sessions': sum, 'MostRecent': max, 'LeastRecent': min, 'PageViews': sum, 'SecsOnSite': sum})).reset_index()
+    return results
+
+
+def rf_compute(df):
+    """
+    Compute the recency and frequency of interaction of users from the BQ data
+    :param df: DF - Pandas dataframe containing user level activity
+    :return: DF - aggregated DF, summarising values for each user
+    """
+    most_recent, least_recent = max(df['MostRecent']), min(df['LeastRecent'])
+    recent_diff = (most_recent - least_recent) / 5
+    df['Recency'] = (df['MostRecent'] - least_recent) / recent_diff
+    df['Recency'] = df['Recency'].round().astype(int)
+    most_frequent, least_frequent = max(df['Sessions']), min(df['Sessions'])
+    frequent_diff = (most_frequent - least_frequent) / 5
+    df['Frequency'] = (df['Sessions'] - least_frequent) / frequent_diff
+    df['Frequency'] = df['Frequency'].round().astype(int)
+    df['RFScore'] = df['Recency'] + df['Frequency']
+    return pd.DataFrame(df)
+
+
+def big_query():
+    print '\nObtaining Big Query Data...\n---------------------------\n'
+    # BQ Project Name
+    project = 'hypnotic-bounty-755'
+    # How many days over which to compute RFM
+    days = get_days()
+    # Specific dates of each day (for BQ table names)
+    dates = get_dates(days)
+    # Aggregate results over given date range
+    agg = aggregate(project, dates)
+    return agg
+
+
+def salesforce(path):
+    print '\nObtaining Salesforce Data\n-------------------------\n'
+    sf = get_df(path + '/Raw/salesforce_2.0')
+    return sf
+
 
 def bq_getdata(project, query):
     if not os.path.isfile('./bigquery_credentials.dat'):
